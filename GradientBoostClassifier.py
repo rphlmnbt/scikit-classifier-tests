2484191726ca532ebf1faff89d12fd5ded303284
rphlmnbt <rphlmnbt@gmail.com>
2022-02-06 14:38:40 +0800

    Created test for Gradient Boost Classifier Algorithm.

--- GradientBoostClassifier.py
+++ GradientBoostClassifier.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from input import *
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 data = pd.read_csv("user.csv")
 X = data.drop(columns=['out'])
 y = data['out']
 
-model = AdaBoostClassifier()
+model = GradientBoostingClassifier()
 model.fit(X, y)
 predictions = model.predict([inputArr])
 predictions_proba = model.predict_proba([inputArr])
 
-print('Ada Boost Classifier Results:')
+print('Gradient Boost Classifier Results:')
 print(predictions)
 print(predictions_proba)
